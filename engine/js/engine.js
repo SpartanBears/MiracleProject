bb0c44aba1cedf06c0c8cad77ff3002e1ee4701d
dvergon <diego.vergara@spartanbears.cl>
2018-03-29 17:24:55 -0300

    Races n Classes

--- engine/js/engine.js
+++ engine/js/engine.js
@@ -3,8 +3,11 @@
 */
 
 //Race example
-var classes['dwarf'] = {
+var races = [];
+
+races['dwarf'] = {
 	name: "Dwarf",
+	lore: "lore",
 	base_stats:{
 		str: 8,
 		agi: 4,
@@ -23,8 +26,106 @@ var classes['dwarf'] = {
 		aspd: 0.5,
 		cspd: 0.1,
 		hit: 25
+	}
+};
+
+//Class example
+/*
+	There will be classes or jobs for the characters.
+	Each class can have an "evolution" or "decendant".
+	Each "step" of the class will be defined in a separate object
+	i.e: ClassA1 -> ClassA2 -> ClassA3
+	classes1["ClassA1"].next = ClassA2;
+	classes2["ClassA2"].next = ClassA3;
+	etc.
+
+	Classes will give the character access to different skills and
+	will recieve bonuses to its stats or attributes.
+*/
+
+var classes = {
+	classes_0:[],
+	classes_1:[],
+	classes_2:[],
+};
+
+classes["classes_0"]["merchant_0"] = {
+	name:"Merchant",
+	lore:"lore",
+	bonus_stats:{
+		str: 0,
+		agi: 0,
+		vit: 10,
+		int: 10,
+		dex: 0,
+		luk: 10
 	},
-}
+	bonus_attributes:{
+		hp: 500,
+		def: 25,
+		mdef: 25,
+		eva: 0,
+		atk: 0,
+		matk: 0,
+		aspd: 0,
+		cspd: 0,
+		hit: 25
+	},
+	next_tier:"classes_1",
+	next_class:"merchant_1",
+};
+
+classes["classes_1"]["merchant_1"] = {
+	name:"Trader",
+	lore:"lore",
+	bonus_stats:{
+		str: 0,
+		agi: 0,
+		vit: 10,
+		int: 10,
+		dex: 0,
+		luk: 20
+	},
+	bonus_attributes:{
+		hp: 500,
+		def: 25,
+		mdef: 25,
+		eva: 0,
+		atk: 0,
+		matk: 0,
+		aspd: 0,
+		cspd: 0,
+		hit: 25
+	},
+	next_tier:"classes_2",
+	next_class:"merchant_2",
+};
+
+classes["classes_2"]["merchant_2"] = {
+	name:"Master Trader",
+	lore:"lore",
+	bonus_stats:{
+		str: 0,
+		agi: 0,
+		vit: 10,
+		int: 10,
+		dex: 0,
+		luk: 20
+	},
+	bonus_attributes:{
+		hp: 500,
+		def: 25,
+		mdef: 25,
+		eva: 0,
+		atk: 0,
+		matk: 0,
+		aspd: 0,
+		cspd: 0,
+		hit: 25
+	},
+	next_tier:"none",
+	next_class:"none",
+};
 
 /* 
 	Characters will cast skills automatically depending on their stats and level.
@@ -34,7 +135,8 @@ var classes['dwarf'] = {
 	i.e: Healing skills have a formula to calculate the amount of HP healed based on the character stats. 
 	The healing skill will allow the character to heal and improve the stats that are involved in the calculation.
 	Healing = matk * (int*0.5>1?int*0.5:1)
-	So, the Healing skill will provide bonus int and matk
+	So, the Healing skill will provide bonus int and matk, instead of having
+	it's own equation.
 */
 
 var skills["dwarf"] = [
