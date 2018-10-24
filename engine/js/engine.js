2bb926cde4199f1c8343e5e4412f2ad4808d8e5d
dvergon <diego.vergara@spartanbears.cl>
2018-10-24 15:50:56 +0100

    Skills
    Shaping up skill workings

--- engine/js/engine.js
+++ engine/js/engine.js
@@ -131,12 +131,22 @@ classes["classes_2"]["merchant_2"] = {
 	Characters will cast skills automatically depending on their stats and level.
 	The skill will be selected randomly from a skill pool.
 	Skills will affect stats or attributes, and wont have an special formula to calculate its effects.
+	Each skill will be classified in one and only one of these categories:
+	- Damage
+	- Healing
+	- Support
+
+	Damage skills will use the default atk/matk, crit, aspd formulas.
+	Healing and Support skills will have an special formula for their categories.
+
+	Healing = matk * (int*0.5>1?int*0.5:1)
+	Support = ?
 
 	i.e: Healing skills have a formula to calculate the amount of HP healed based on the character stats. 
 	The healing skill will allow the character to heal and improve the stats that are involved in the calculation.
 	Healing = matk * (int*0.5>1?int*0.5:1)
 	So, the Healing skill will provide bonus int and matk, instead of having
-	it's own equation.
+	it's own formula.
 
 	Skills will be sorted/grouped by race or class/job
 */
